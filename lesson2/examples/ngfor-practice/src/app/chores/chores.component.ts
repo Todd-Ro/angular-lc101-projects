aa637667b98dd5e4012adf903d41b882969eee87
toddr <todd.romkema@gmail.com>
2021-11-22 20:46:11 -0600

    Got *ngfor working

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+let yestTasks: string[] = ['Empty dishwasher', 'Start LaunchCode prep work', 'Buy groceries'];
+let yesterdChoreObj = {title: "Yesterday's Chores", tasks: yestTasks};
+let todayTasks: string[] = ['Load dishwasher', 'Finish LaunchCode prep work', 'Buy the groceries you forgot'];
+let todayChoreObj = {title: "Today's Chores", tasks: todayTasks};
+let tomorrowTasks: string[] = ['Empty dishwasher AGAIN', 'Play with LaunchCode practice code', 'Groceries AGAIN'];
+let tomorChoreObj = {title: "Tomorrow's Chores", tasks: tomorrowTasks};
+
 @Component({
    selector: 'chores',
    templateUrl: './chores.component.html',
    styleUrls: ['./chores.component.css']
 })
+
+
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores: object[] = [
+      yesterdChoreObj, todayChoreObj, tomorChoreObj
+   ];
+   todoTitles = ["Yesterday's Chores", "Today's Chores", 
+         "Tomorrow's Chores", "Next Week's Daily Chores"];
 
    constructor() { }
 
